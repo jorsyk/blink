c5f2d1a444a001f7e0f0de539c4359cc4c73c33e
jorsyk <trubchik36@gmail.com>
2025-01-05 18:04:48 +0300

    Created PostListCreateView in post/views;

--- post/views.py
+++ post/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostListCreateView(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
