f9e2950a220acc0dbcf88137d41cb0d3c5bd24d5
jorsyk <trubchik36@gmail.com>
2025-01-05 17:22:38 +0300

    Created table User in user/models; Create Post, Like in post/models;

--- post/models.py
+++ post/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+
+from user.models import User
+
+
+class Post(models.Model):
+    """Модель поста соцсети"""
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    content = models.TextField()
+    image = models.ImageField(upload_to='post_images', blank=True, null=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+
+class Like(models.Model):
+    """Модель лайков на посте в соцсети"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
--- user/models.py
+++ user/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    decription = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/')
