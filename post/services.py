6f31bbeeda9c456955aaddfb579afb2d6c2cff80
jorsyk <trubchik36@gmail.com>
2025-01-08 21:07:58 +0300

    refactoring: services.py/post

--- post/services.py
+++ post/services.py
@@ -1,19 +1,20 @@
-from django.shortcuts import get_object_or_404
-from .models import Post, Like
+from .repositories import PostRepository, LikeRepository
 
 
 def get_post_by_id(post_id):
-    """Получить пост по его ID"""
-    return get_object_or_404(Post, id=post_id)
+    """
+    Получить пост по его ID.
+    """
+    return PostRepository.get_by_id(post_id)
 
 
 def toggle_like(user, post):
     """
     Логика добавления/удаления лайка.
-    Возвращает сообщение о статусе действия
+    Возвращает сообщение о статусе действия.
     """
-    like, created = Like.objects.get_or_create(user=user, post=post)
-    if not created:
-        like.delete()
-        return 'Лайк удален'
-    return 'Лайк поставлен'
+    like, create = LikeRepository.get_or_create(user, post)
+    if not create:
+        LikeRepository.delete(like)
+        return 'Like removed'
+    return 'Post liked'
